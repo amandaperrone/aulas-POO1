2385b5dd2d5d173243e39272c99a0e3fdeeeea96
Amanda Perrone <amanda_degrande@yahoo.com.br>
2020-09-16 22:48:11 -0300

    aula 16-09-2020

---
+++ aula5/exercicio/Carro.java
@@ -0,0 +1,85 @@
+package exercicio;
+
+public class Carro {
+
+    // Criação de atributos
+    private int codigo;
+    private double velocidadeKmPorHora;
+    private double distanciaPercorridaEmMetros;
+    private double tanque;
+    private double consumo;
+
+    public double getTanque() {
+        return tanque;
+    }
+
+    public boolean abastecer(double combustivel) {
+        if (combustivel < 0)
+            return false;
+
+        if(combustivel > 50)
+            this.tanque = 50;
+        else if(combustivel>0)
+            this.tanque = combustivel;
+
+        return true;
+    }
+
+    public double moverEx(final int segundos){
+
+        if(tanque > 0)
+        {
+            double dist = (velocidadeKmPorHora/3.6)*(segundos);
+            tanque -= (dist*0.001)/consumo;
+            this.distanciaPercorridaEmMetros += dist;
+            return dist;
+        }
+        else
+        {
+            return 0;
+        }
+        
+    }
+
+    public double getDistanciaPercorridaEmMetros() {
+        return distanciaPercorridaEmMetros;
+    }
+
+    public void setDistanciaPercorridaEmMetros(double distanciaPercorridaEmMetros) {
+        this.distanciaPercorridaEmMetros = distanciaPercorridaEmMetros;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getVelocidadeKmPorHora() {
+        return velocidadeKmPorHora;
+    }
+
+    public void setVelocidadeKmPorHora(double velocidadeKmPorHora) {
+        this.velocidadeKmPorHora = velocidadeKmPorHora;
+    }
+
+    public double getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(double consumo) {
+        this.consumo = consumo;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Carro [codigo=" + codigo + ", consumo=" + consumo + ", distanciaPercorridaEmMetros="
+                + distanciaPercorridaEmMetros + ", tanque=" + tanque + ", velocidadeKmPorHora=" + velocidadeKmPorHora
+                + "]";
+    }
+    
+}
---
+++ aula5/exercicio/CorridaEx.java
@@ -0,0 +1,62 @@
+package exercicio;
+
+import java.util.ArrayList;
+
+public class CorridaEx {
+    public static void main(final String[] args) {
+
+        final ArrayList<Carro> carros = new ArrayList<Carro>();
+        final ArrayList<Carro> finalCorrida = new ArrayList<Carro>();
+        final ArrayList<Carro> desclassificados = new ArrayList<Carro>();
+
+        final int qtdCarros = 100;
+        final int tamanhoPista = 10000;
+
+        for (int i = 0; i < qtdCarros; i++) {
+            final Carro c = new Carro();
+            c.setCodigo(i);
+            c.setConsumo(Math.random() * 10 + 5); // Sorteia entre 5 e 15
+            c.abastecer(Math.random() * 45 + 5); // Sorteia entre 5 e 50
+            carros.add(c);
+        }
+
+        // Tem carro na corrida ainda?
+        while (carros.size() > 0) {
+            // Mover todos os carros
+            for (final Carro c : carros) {
+                c.setVelocidadeKmPorHora(Math.random() * 200);
+
+                // Conseguiu mover?
+                if (c.moverEx(1) > 0) {
+                    if (c.getDistanciaPercorridaEmMetros() > tamanhoPista) {
+                        System.out.println("Carro " + c.getCodigo() + " terminou a corrida.");
+                        finalCorrida.add(c);
+                    }
+
+                } else {
+                    System.out.println("Carro " + c.getCodigo() + " desclassificado.");
+                    desclassificados.add(c);
+                }
+            }
+
+            // Remove os carros desclassificados ou que já terminaram a corrida
+            carros.removeAll(finalCorrida);
+            carros.removeAll(desclassificados);
+        } // while
+
+        System.out.println("CLASSIFICAÇÃO FINAL");
+        int cont = 1;
+        for (final Carro c : finalCorrida) {
+            System.out.println(cont + "o. " + c.getCodigo());
+            cont++;
+        }
+
+        System.out.println("DESCLASSIFICADOS");
+        for (final Carro c : desclassificados)
+        {
+            System.out.println(cont + "o. " + c.getCodigo());
+            cont++;
+        }
+
+    }
+}
